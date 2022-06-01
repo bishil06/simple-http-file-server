972187382ca2a6990aae0f16597117a52c1d1cad
HyunJae Lee <bishil06@naver.com>
2022-06-02 04:42:14 +0900

    add new usage - send directory in files

--- client/index.mjs
+++ client/index.mjs
@@ -1,22 +1,12 @@
 import { request } from 'http'
 import { createGzip } from 'zlib'
-import { createReadStream } from 'fs'
+import { createReadStream, lstatSync } from 'fs'
 import { basename } from 'path'
+
+import getFileList from './getFileList.mjs'
 
 const filename = process.argv[2]
 const serverHost = process.argv[3]
-
-const httpRequestOptions = {
-  hostname: serverHost,
-  port: 8080,
-  path: '/',
-  method: 'PUT',
-  headers: {
-    'Content-Type': 'application/octet-stream',
-    'Content-Encoding': 'gzip',
-    'X-Filename': basename(filename)
-  }
-}
 
 function sendFile(fileName, httpOptions) {
   const req = request(httpOptions, (res) => {
@@ -37,4 +27,37 @@ function sendFile(fileName, httpOptions) {
   })
 }
 
-await sendFile(filename, httpRequestOptions)
+if (lstatSync(filename).isDirectory()) {
+  for await (const p of getFileList(filename)) {
+    const httpRequestOptions = {
+      hostname: serverHost,
+      port: 8080,
+      path: '/',
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Content-Encoding': 'gzip',
+        'X-Filename': basename(p)
+      }
+    }
+
+    await sendFile(p, httpRequestOptions)
+  }
+}
+else {
+  const httpRequestOptions = {
+    hostname: serverHost,
+    port: 8080,
+    path: '/',
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Content-Encoding': 'gzip',
+      'X-Filename': basename(filename)
+    }
+  }
+
+  await sendFile(filename, httpRequestOptions)
+}
+
+
